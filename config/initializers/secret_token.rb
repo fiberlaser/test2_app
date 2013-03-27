550f927661b2ded8af1d70a970cff2f96fed5295
fiberlaser <fiber.laser@live.com>
2013-03-26 21:41:00 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Test2App::Application.config.secret_token = 'c2cabb0d6847842443a61d46accca8c9fef4b02e44b06ef5a861ad6c93ed0d1c901e5f41409bfd6666231028f626ff5d3b7317db1649d6ff7f6ceb3636881d12'
